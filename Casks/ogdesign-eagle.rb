88e6c87ab074d9f93726b3d1e55a384076b48af8
Allen Bu <1285689489@qq.com>
2022-08-10 09:27:19 +0800

    update namespace

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,4 +1,4 @@
-cask "ogdesign-eagle" do
+cask "nagatebu-ogdesign-eagle" do
   arch = Hardware::CPU.intel? ? "" : "M1-"
 
   version "3.0,9"
--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,4 +1,4 @@
-cask "typora" do
+cask "nagatebu-typora" do
   version "1.3.7"
   sha256 "88731f0eace5784915a9e221724eca5c7460076d43290dd3444e8ad1c5e5ba5e"
 
