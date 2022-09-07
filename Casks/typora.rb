87a62896ceb4115ad9ba9400b7c49de1633db9cd
NagateBu <AllenBu0415@outlook.com>
2022-09-07 17:30:01 +0800

    Update typora.rb

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,66 +1,30 @@
-{
-    "token": "typora",
-    "full_token": "typora",
-    "tap": "homebrew/cask",
-    "name": [
-        "Typora"
-    ],
-    "desc": "Configurable document editor that supports Markdown",
-    "homepage": "https://typora.io/",
-    "url": "http://typora-download.oss-cn-shanghai.aliyuncs.com/mac/Typora-1.4.4.dmg",
-    "appcast": null,
-    "version": "1.4.4",
-    "versions": {},
-    "installed": null,
-    "outdated": false,
-    "sha256": "362529796594880534667d2dade62bf94ea0f8751476b99e263e64fab69cdd91",
-    "artifacts": [
-        {
-            "app": [
-                "Typora.app"
-            ]
-        },
-        {
-            "zap": [
-                {
-                    "trash": [
-                        "~/Library/Application Support/abnerworks.Typora",
-                        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl*",
-                        "~/Library/Application Support/Typora",
-                        "~/Library/Caches/abnerworks.Typora",
-                        "~/Library/Cookies/abnerworks.Typora.binarycookies",
-                        "~/Library/Preferences/abnerworks.Typora.plist",
-                        "~/Library/Saved Application State/abnerworks.Typora.savedState",
-                        "~/Library/WebKit/abnerworks.Typora"
-                    ]
-                }
-            ]
-        }
-    ],
-    "caveats": null,
-    "depends_on": {
-        "macos": {
-            ">=": [
-                "10.13"
-            ]
-        }
-    },
-    "conflicts_with": null,
-    "container": null,
-    "auto_updates": true,
-    "variations": {},
-    "analytics": {
-        "install": {
-            "30d": {
-                "typora": 681
-            },
-            "90d": {
-                "typora": 1914
-            },
-            "365d": {
-                "typora": 11680
-            }
-        }
-    },
-    "generated_date": "2022-09-07"
-}
+cask "typora" do
+  version "1.4.4"
+  sha256 "362529796594880534667d2dade62bf94ea0f8751476b99e263e64fab69cdd91"
+
+  url "http://typora-download.oss-cn-shanghai.aliyuncs.com/mac/Typora-#{version}.dmg"
+  name "Typora"
+  desc "Configurable document editor that supports Markdown"
+  homepage "https://typora.io/"
+
+  livecheck do
+    url "https://www.typora.io/download/dev_update.xml"
+    regex(/Typora-(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Typora.app"
+
+  zap trash: [
+    "~/Library/Application Support/abnerworks.Typora",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl*",
+    "~/Library/Application Support/Typora",
+    "~/Library/Caches/abnerworks.Typora",
+    "~/Library/Cookies/abnerworks.Typora.binarycookies",
+    "~/Library/Preferences/abnerworks.Typora.plist",
+    "~/Library/Saved Application State/abnerworks.Typora.savedState",
+    "~/Library/WebKit/abnerworks.Typora",
+  ]
+end
